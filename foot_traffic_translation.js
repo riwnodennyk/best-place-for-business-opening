53ee9b73d186c26b1af46de71290fa536ac35371
andrii-lavrinenko-gulliver <104841347+andrii-lavrinenko-gulliver@users.noreply.github.com>
2025-03-21 19:00:16 +0200

    Translate metadata into langs

--- foot_traffic_translation.js
+++ foot_traffic_translation.js
@@ -19,6 +19,13 @@ const userLang = navigator.language.substring(0, 2);
 let translation = translations[userLang] || translations["en"];
 
 document.querySelector('meta[name="description"]').setAttribute("content", translate("metaDescription"));
+document.querySelector('meta[name="keywords"]').setAttribute("content", translate("keywords"));
+document.querySelector('meta[property="og:title"]').setAttribute("content", translate("og_title"));
+document.querySelector('meta[property="og:description"]').setAttribute("content", translate("og_description"));
+document.querySelector('meta[name="twitter:title"]').setAttribute("content", translate("twitter_title"));
+document.querySelector('meta[name="twitter:description"]').setAttribute("content", translate("twitter_description"));
+
+
 if (document.getElementById("map")) document.getElementById("map").setAttribute("data-placeholder", translate("mapPlaceholder"));
 
 export { translate }
